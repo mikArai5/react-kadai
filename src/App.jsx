0effe45ddfec0a647f6f508fde25e59f4049d216
荒井美香 <>
2024-02-23 15:57:30 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import TopPage from "./pages/TopPage";
+
+function App() {
+
+  return (
+    <div className='App'>
+    <TopPage />
+    <h1>aaaa</h1>
+    </div>
+  );
+};
+
+export default App
---
+++ src/pages/TopPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./TopPage.css";
+
+export const TopPage = () => {
+    return (
+        <>
+        <h1>こんにちは</h1>
+        <p>表示されない</p>
+        </>
+    );
+};
